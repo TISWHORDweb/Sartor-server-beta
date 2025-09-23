9a7ec3886c4f011c0ed1155e90372f620d869313
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2025-09-23 00:41:44 -0700

    adding origin ***

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ global.console.log = function (...args) {
 // });
 
 app.use(cors({
-    origin: ['https://crm.sartor.ng', 'https://crm.sartor.ng', 'http://localhost:5173/'], 
+    origin: ['https://crm.sartor.ng', 'https://crm.sartor.ng', 'http://localhost:5173', 'https://sartor-crm.onrender.com'], 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
