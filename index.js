224eea8ca878ab0941bf7097550b3087083f0a41
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2025-10-02 18:51:28 -0700

    congfig token header ***

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(cors({
     origin: ['https://crm.sartor.ng', 'https://crm.sartor.ng', 'http://localhost:5173', 'https://sartor-crm.onrender.com'], 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization', 's-token']
 }));
 
 app.options('*', cors());
